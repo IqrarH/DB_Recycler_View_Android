d108b66683a957f30d972b318f5955462359add6
IqrarH <iqrarhussain471@gmail.com>
2021-12-19 22:28:23 +0500

    getAllStudents function added

--- app/src/main/java/com/example/db_recycler_view_android/DbHelper.java
+++ app/src/main/java/com/example/db_recycler_view_android/DbHelper.java
@@ -2,10 +2,13 @@ package com.example.db_recycler_view_android;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class DbHelper extends SQLiteOpenHelper {
 
@@ -42,5 +45,28 @@ public class DbHelper extends SQLiteOpenHelper {
         db.insert(STUDENT_TABLE, null, cv);
         db.close();
 
+    }
+
+    public ArrayList<StudentModel> getAllStudents() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+
+        ArrayList<StudentModel> studentArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+
+                studentArrayList.add(new StudentModel(cursorCourses.getString(1),
+                        cursorCourses.getInt(2),
+                        cursorCourses.getInt(3) == 1 ? true : false));
+            } while (cursorCourses.moveToNext());
+
+        }
+
+        cursorCourses.close();
+        return studentArrayList;
     }
 }
