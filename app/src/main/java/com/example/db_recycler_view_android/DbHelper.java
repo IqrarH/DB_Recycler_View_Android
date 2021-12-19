f05aedd5725b7c8cf570d673477d41a159aa89ec
IqrarH <iqrarhussain471@gmail.com>
2021-12-19 22:25:18 +0500

    DB Helper Methods Implemented

--- app/src/main/java/com/example/db_recycler_view_android/DbHelper.java
+++ app/src/main/java/com/example/db_recycler_view_android/DbHelper.java
@@ -25,7 +25,8 @@ public class DbHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(db);
     }
 }
